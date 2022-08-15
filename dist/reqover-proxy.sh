724aa422068008803323e5a68ddeead48cd952c5
s.pirohov <sergey.pirogov@gmail.com>
2022-08-15 16:45:01 +0300

    add reqover-proxy.sh

---
+++ dist/reqover-proxy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PROJECT_TOKEN=4zjud4ttejxk
+SWAGGER_URL=https://petstore.swagger.io/v2/swagger.json
+BASE_PATH=/2
+BRANCH=Proxy
+TARGET_SERVICE_URL=https://petstore.swagger.io
+
+docker run -it -p 8080:8080 \
+  -e PROJECT_TOKEN=$PROJECT_TOKEN \
+  -e SWAGGER_URL=$SWAGGER_URL \
+  -e BASE_PATH=$BASE_PATH \
+  -e BRANCH=$BRANCH \
+  reqover/reqover-proxy --mode reverse:$TARGET_SERVICE_URL
